9dfc173828d5cfbf497ef63325857aec4c2be582
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-06-04 18:33:59 -0500

    studio solutions attempt

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -7,16 +7,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 
 @Controller
 public class EventController {
 
     @RequestMapping(value= "events")
     public String getEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Coding Jamboree");
-        events.add("Arch Coders");
-        events.add("FOSS4G Coding");
+        HashMap<String, String> events = new HashMap<>();
+        events.put("Coding Jamboree", "This is a fun coding event with dancing");
+        events.put("Arch Coders", "St Louis area arch coders unite!");
+        events.put("FOSS4G Coding", "Open source rules!");
 
         model.addAttribute("events", events);
 
