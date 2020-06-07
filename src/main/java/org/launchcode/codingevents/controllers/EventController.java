2c2737cd863c2adfba4285db50a37b08fb8cf4b8
Brookelynn Constant <Brookelynn.constant@gmail.com>
2020-06-06 20:15:06 -0500

    created Event class

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.launchcode.codingevents.models.Event;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,7 +15,8 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+//    private static List<String> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
     //list is coding to an interface type
 
     @GetMapping
@@ -24,6 +26,7 @@ public class EventController {
 //        events.add("Arch Coders");
 //        events.add("FOSS4G Coding");
 //        model.addAttribute("events", events);
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
 
@@ -31,14 +34,16 @@ public class EventController {
 
     //lives at /events
     @GetMapping(value = "create")
-    public String renderCreateEvent(){
+    public String renderCreateEvent(Model model){
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     //lives at /events/create
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+//        events.add(eventName);
+        events.add(new Event(eventName));
         return "redirect:"; //300-level http response and instructs browser to redirect:/events
 
     }
